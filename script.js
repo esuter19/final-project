36f04eb32efaec5959ea1134710b281cb9052f90
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-05-01 18:31:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
+require(["esri/Map", "esri/views/MapView", "esri/WebMap"], function (Map, MapView, WebMap) {
         var map = new Map({
           basemap: "topo-vector"
         });
-
+        
+        var wm = new WebMap({
+          portalItem: {
+            id: "9e4a2dda743b44faa7b9b79f648bbc9e"
+          }
+        });
+  
         var view = new MapView({
           container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
+          map: wm,
+          zoom: 2,
+          center: [0, 0] // longitude, latitude
         });
-      });
+
+      function addLayer(layer){
+        map.add(layer);
+      }
+      
+      function rejection(error){
+        console.log("Layer failed to load: ", error);
+      }
+});
