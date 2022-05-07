50875ad7476a1570ef31127cba657b277f62f417
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-05-06 22:26:08 -0500

    copied over changes from codepen

--- script.js
+++ script.js
@@ -1,26 +1,82 @@
-require(["esri/Map", "esri/views/MapView", "esri/WebMap"], function (Map, MapView, WebMap) {
+require(["esri/Map", 
+         "esri/views/MapView",  
+         "esri/layers/Layer",
+         "esri/layers/GeoJSONLayer",
+         'esri/widgets/Legend'], function (Map, MapView,  Layer, GeoJSONLayer, Legend) {
         var map = new Map({
           basemap: "topo-vector"
         });
         
-        var wm = new WebMap({
-          portalItem: {
-            id: "9e4a2dda743b44faa7b9b79f648bbc9e"
-          }
+        var view = new MapView({
+          map: map,
+          container: "viewDiv",
+          zoom: 2,
+          center: [0, 0]
         });
   
-        var view = new MapView({
-          container: "viewDiv",
-          map: wm,
-          zoom: 2,
-          center: [0, 0] // longitude, latitude
+        var countries_renderer = {
+          type:"simple",
+          symbol:{type:"simple-fill"},
+          visualVariables: [{
+            type: "color",
+            field: "pctg_mil",
+            stops: [{ value: 2.5, color: "#FF001F" },
+          { value: 0, color: "#FFFFFF" }]
+          }]
+        }
+        
+        var popup_template = {
+        title: "{sovereignt}",
+        content: [{
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "active_mil",
+            label: "Active Military Personnel: ",
+            visible: true,
+            format: {
+              digitSeparator: true,
+              places: 0
+            }
+          }, {
+            fieldName: "tot_mil",
+            label: "Total Military Personnel: ",
+            visible: true,
+            format: {
+              digitSeparator: true,
+              places: 0
+            }
+          }, {
+            fieldName: "pop_est",
+            label: "Estimated Population: ",
+            visible: true,
+            format: {
+              digitSeparator: true,
+              places: 0
+            }
+          }, {
+            fieldName: "pctg_mil",
+            label: "Population Percentage Military: ",
+            visible: true,
+            format: {
+              digitSeparator: true,
+              places: 2
+            }
+          }]
+        }]
+      };
+  
+        var countries = new GeoJSONLayer({
+            url: "https://raw.githubusercontent.com/esuter19/final-project/main/md_data_export.json",
+            renderer: countries_renderer,
+            popupTemplate: popup_template
+            });
+        map.add(countries);
+  
+        
+  
+        const legend = new Legend({
+          view: view
         });
 
-      function addLayer(layer){
-        map.add(layer);
-      }
-      
-      function rejection(error){
-        console.log("Layer failed to load: ", error);
-      }
+        view.ui.add(legend, "bottom-left");
 });
